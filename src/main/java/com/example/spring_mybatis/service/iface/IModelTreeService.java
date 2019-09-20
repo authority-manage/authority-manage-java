dde1c06fba2f09f88380bbf02afe7beb0cce7f32
苞米地教父 <yugongzhou@foxmail.com>
2019-09-20 15:05:08 +0800

    Revert "第一版"
    This reverts commit aefc5fc899503bf01150cf41857f11b09757f686.

--- src/main/java/com/example/spring_mybatis/service/iface/IModelTreeService.java
+++
@@ -1,9 +0,0 @@
-package com.example.spring_mybatis.service.iface;
-
-import java.util.List;
-
-import com.example.spring_mybatis.model.ModelTree;
-
-public interface IModelTreeService {
-	List<ModelTree> getAllModel(String userId);
-}
--- src/main/java/com/example/spring_mybatis/service/iface/RoleInfoService.java
+++
@@ -1,9 +0,0 @@
-package com.example.spring_mybatis.service.iface;
-
-import java.util.List;
-
-import com.example.spring_mybatis.model.RoleInfo;
-
-public interface RoleInfoService {
-
-}
